afa96dea9652dbe3391e4339cebdfef15008eb5d
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-05 12:44:56 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -34,10 +34,13 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
-  
+  handleOnKeyDown =()=>{
+    return 
+  }
   render() {
     return (
       <canvas 
+        onKeyDown={}
         onClick={this.handleOnClick}
         onMouseMove={this.handleMouseMove}
         width='900'
